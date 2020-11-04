6a23eeac22108b1088f2e32a587b796b9602ebec
Yuwetha <yuthiesdesigners10@gmail.com>
2020-11-05 00:42:43 +0530

    nov5 edits

---
+++ frontend/build/precache-manifest.60ae815511e84d25b065ba06cdbb46df.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91bbd5e460b853e0eb12b0108bf8ace6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34ed96b5ba29ce87d53f",
+    "url": "/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "32fd4d8f65a25b3615c2",
+    "url": "/static/css/main.2a55b814.chunk.css"
+  },
+  {
+    "revision": "34ed96b5ba29ce87d53f",
+    "url": "/static/js/2.cdc9c51d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.cdc9c51d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32fd4d8f65a25b3615c2",
+    "url": "/static/js/main.dca05927.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "da35eb97e136d01f3495884e58fb0a1e",
+    "url": "/static/media/Mobile-login-01.da35eb97.png"
+  },
+  {
+    "revision": "b29b65674468ba9932ce07208a126d5e",
+    "url": "/static/media/address.b29b6567.png"
+  },
+  {
+    "revision": "82d8758c4aa86d41c191f230aebac058",
+    "url": "/static/media/ceo1.82d8758c.png"
+  },
+  {
+    "revision": "93dec20eb216f9afd121f8b2183e969a",
+    "url": "/static/media/ceo2.93dec20e.png"
+  },
+  {
+    "revision": "9424400c2ded0d7ed8acdff8d3583e0a",
+    "url": "/static/media/ceo3.9424400c.png"
+  },
+  {
+    "revision": "a58d7b3c7ac62271aec5598d6b6f092f",
+    "url": "/static/media/ceo4.a58d7b3c.png"
+  },
+  {
+    "revision": "164918eba5c6d6f88453c0a5a604bac5",
+    "url": "/static/media/cover.164918eb.jpg"
+  },
+  {
+    "revision": "e66bedffeb2a3a138df356ba12a7aeac",
+    "url": "/static/media/coverpro.e66bedff.png"
+  },
+  {
+    "revision": "883a52a7e5a7d28f9c4245ad00939191",
+    "url": "/static/media/features.883a52a7.png"
+  },
+  {
+    "revision": "e5cc4d5149eb7515efe790e457747231",
+    "url": "/static/media/features1.e5cc4d51.png"
+  },
+  {
+    "revision": "dade37721478b4cc60cfa37945b43e2d",
+    "url": "/static/media/features2.dade3772.png"
+  },
+  {
+    "revision": "4bc666271d36c4732968cc556bd73707",
+    "url": "/static/media/logo.4bc66627.png"
+  },
+  {
+    "revision": "dd21187c1fbf7eef6bf15250b5f9783b",
+    "url": "/static/media/payment.dd21187c.png"
+  },
+  {
+    "revision": "436bc7cb8324f8e63c0040a7773fa869",
+    "url": "/static/media/productcarousel1.436bc7cb.png"
+  },
+  {
+    "revision": "40e837cf27b1ee244e5762b3faf7d69e",
+    "url": "/static/media/productcarousel2.40e837cf.png"
+  },
+  {
+    "revision": "9a38e76767ff6902f50cbd4bcbbea359",
+    "url": "/static/media/productcarousel3.9a38e767.png"
+  },
+  {
+    "revision": "94bc9b0129915300fbce3cb19c95dcb6",
+    "url": "/static/media/workflow.94bc9b01.png"
+  },
+  {
+    "revision": "6c406d12f562acc14b3a2fab307d772a",
+    "url": "/static/media/yuthies logo.6c406d12.png"
+  }
+]);
